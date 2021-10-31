daa7902b997e6b536f6206b6cf2e003918c93cd0
Joakim Johansson <me@joakimjohansson.se>
2021-10-31 16:14:18 +0100

    Added dataset module for snabbdom

--- src.js
+++ src.js
@@ -5,6 +5,7 @@ import {
     classModule,
     propsModule,
     styleModule,
+    datasetModule,
     attributesModule,
     eventListenersModule,
     h,
@@ -17,5 +18,6 @@ window.h = h
 window.snabbdom_class = classModule
 window.snabbdom_props = propsModule
 window.snabbdom_style = styleModule
+window.snabbdom_dataset = datasetModule
 window.snabbdom_attributes = attributesModule
 window.snabbdom_eventlisteners = eventListenersModule
